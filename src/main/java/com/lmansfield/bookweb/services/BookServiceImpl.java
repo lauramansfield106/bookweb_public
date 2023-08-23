d5a4816f8c7a0e149cc6731090e736e29a50459a
Laura Mansfield <lauramansfield106@gmail.com>
2023-08-22 20:19:46 -0400

    Adds implementation class of BookService

---
+++ src/main/java/com/lmansfield/bookweb/services/BookServiceImpl.java
@@ -0,0 +1,39 @@
+package com.lmansfield.bookweb.services;
+
+import java.util.List;
+
+import com.lmansfield.bookweb.model.Book;
+
+public class BookServiceImpl implements BookService {
+
+	@Override
+	public String createBook(Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateBook(Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteBook(Long bookId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Book getBook(Long bookId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
