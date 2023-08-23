a8c3fb0d6b4d990a381add7c9c84549256b90fc3
Laura Mansfield <lauramansfield106@gmail.com>
2023-08-22 20:43:38 -0400

    Cleans up unnecessary comments

--- src/main/java/com/lmansfield/bookweb/services/BookService.java
+++ src/main/java/com/lmansfield/bookweb/services/BookService.java
@@ -6,21 +6,13 @@ import com.lmansfield.bookweb.model.Book;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.lmansfield.bookweb.model.Book;
-//import com.lmansfield.bookweb.repositories.BookRepository;
-//
-//@Service
-////service = service component class. tells us this class is a string bean, needs to be instantiated
-////and paired with bean/autowired annotation
+
 public interface BookService {
 	public String createBook(Book book);
 	public String updateBook(Book book);
 	public String deleteBook(Long bookId);
 	public Book getBook(Long bookId);
+	
 	/***
 	 * Get all books in the database
 	 * @return List of books
@@ -29,25 +21,3 @@ public interface BookService {
 	
 }
 
-//	
-//	private final BookRepository bookRepo;
-//	
-//	public BookService() {
-//		this.bookRepo = null;}
-//	
-//	@Autowired
-//	public BookService(BookRepository bookRepo) {
-//		this.bookRepo = bookRepo;
-//	}
-//	
-//	
-//	public List<Book> getBooks() {
-//		return bookRepo.findAll();
-//	
-//	}
-//
-//	public void addNewBook(Book book) {
-//		bookRepo.save(book);
-//		
-//	}
-//}
