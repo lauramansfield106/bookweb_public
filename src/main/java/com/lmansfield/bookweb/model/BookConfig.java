14b28462715aa952d198be86616176a7d39d1084
Laura Mansfield <lauramansfield106@gmail.com>
2023-08-24 11:13:32 -0400

    Adds folders for js/css and adds index.html

--- src/main/java/com/lmansfield/bookweb/model/BookConfig.java
+++ src/main/java/com/lmansfield/bookweb/model/BookConfig.java
@@ -10,14 +10,14 @@ import com.lmansfield.bookweb.repositories.BookRepository;
 public class BookConfig {
 
 
-	@Bean
-	CommandLineRunner commandLineRunner(BookRepository repo) {
-		return args -> {		
-			Book mybook = new Book("A Tree Grows in Brooklyn", "Betty Smith", 5, 0L);
-			repo.save(mybook);
-
-		};
-	}
+//	@Bean
+//	CommandLineRunner commandLineRunner(BookRepository repo) {
+//		return args -> {		
+//			Book mybook = new Book("A Tree Grows in Brooklyn", "Betty Smith", 5, 0L);
+//			repo.save(mybook);
+//
+//		};
+//	}
 }
 
 
